4a6085b74e9096a877b00b8be59c863053c8fe7e
technoch1ef <oleksandr.ovcharov@outreach.io>
2025-06-05 13:37:13 +0200

    custom astrodark-vivid theme

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -33,11 +33,61 @@ return {
     },
   },
 
-  {
-    "scottmckendry/cyberdream.nvim",
-    name = "cyberdream",
-    config = function()
-      vim.cmd.colorscheme "cyberdream"
-    end,
-  }
+  -- {
+  --   "scottmckendry/cyberdream.nvim",
+  --   name = "cyberdream",
+  --   config = function()
+  --     vim.cmd.colorscheme "cyberdream"
+  --   end,
+  -- },
+  -- {
+  --   "tpope/vim-vividchalk",
+  --   name = "vividchalk",
+  --   config = function()
+  --     vim.cmd.colorscheme "vividchalk"
+  --   end,
+  -- },
+	{
+		"AstroNvim/astrotheme",
+		-- https://github.com/AstroNvim/astrotheme/blob/main/lua/astrotheme/palettes/astrodark.lua
+		opts = {
+		  palettes = {
+			  astrodark = {
+				  ui = {
+            base         = "#000000",
+          -- You can also override these so splits/floats/etc match nicely:
+            inactive_base = "#000000",
+            float         = "#000000",
+            split         = "#000000",
+
+				    red = "#ff8191",
+				    orange = "#ffa942",
+				    yellow = "#ffcf32",
+				    green = "#b0ff7f",
+				    cyan = "#5efff5",
+				    blue = "#57b6ff",
+				    purple = "#eba0ff",
+				  },
+				  syntax = {
+				    red = "#ff8191",
+            orange = "#ffa942",
+            yellow = "#ffcf32",
+            green = "#b0ff7f",
+            cyan = "#5efff5",
+            purple = "#eba0ff",
+            text = "#ADB0BB",
+            comment = "#696C76",
+            mute =  "#595C66",
+
+				  },
+
+				  term = {
+				   black = "#000000",
+				   bright_red = "#000000"
+				  },
+			  },
+		  },
+		},
+	},
+
 }
