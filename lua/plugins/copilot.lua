40f17887cd9d73b68f68acf9801badbe5a47db2b
technoch1ef <oleskandr.sheff3rd@gmail.com>
2025-10-09 16:33:16 +0200

    fix: use node 22.x for copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -4,7 +4,7 @@ return {
     cmd = "Copilot",
     event = "InsertEnter",
     opts = {
-      copilot_node_command = vim.fn.expand("~/.asdf/installs/nodejs/20.16.0/bin/node"),
+      copilot_node_command = vim.fn.expand("~/.asdf/installs/nodejs/22.14.0/bin/node"),
       suggestion = { enabled = false },
       panel = { enabled = false },
       filetypes = {
